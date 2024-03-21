f9319d1f2184fccc4a853ffe61497001a44382f6
LucasXColaco <133161894+LucasXColaco@users.noreply.github.com>
2024-03-21 19:38:27 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 function openMenu() {
   menu.style.display = "flex";
   menuicon.style.display = "none";
+  document.body.style.overflowY = 'hidden'
 }
 
 function closeMenu() {
   menu.style.display = "none";
   menuicon.style.display = "block";
+  document.body.style.overflowY = 'scroll'
 }
