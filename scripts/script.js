25ff876e36fbdac999e38e34d3b14adbf457cbbb
LucasXColaco <133161894+LucasXColaco@users.noreply.github.com>
2024-03-21 19:48:35 -0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,13 @@
 function openMenu() {
   menu.style.display = "flex";
   menuicon.style.display = "none";
-  document.body.style.overflowY = 'hidden'
+  document.body.style.overflowY = "hidden";
+  whatsapp.style.display = "none";
 }
 
 function closeMenu() {
   menu.style.display = "none";
   menuicon.style.display = "block";
-  document.body.style.overflowY = 'scroll'
+  document.body.style.overflowY = "visible";
+  whatsapp.style.display = "block";
 }
