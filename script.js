a8c884d7f1ff1443113b80a2ad94bc1d1be4218c
LucasXColaco <133161894+LucasXColaco@users.noreply.github.com>
2024-03-21 19:33:21 -0300

    teste

---
+++ script.js
@@ -0,0 +1,9 @@
+function openMenu() {
+  menu.style.display = "flex";
+  menuicon.style.display = "none";
+}
+
+function closeMenu() {
+  menu.style.display = "none";
+  menuicon.style.display = "block";
+}
